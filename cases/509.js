854d91138edcbfbaa156e34ad468cfa2dbb70a96
andre <andregomars@gmail.com>
2020-05-26 22:37:31 -0700

    update 509

--- cases/509.js
+++ cases/509.js
@@ -6,9 +6,38 @@ F(N) = F(N - 1) + F(N - 2), for N > 1.
 Given N, calculate F(N).
 */
 
+// var fib = function(n) {
+//   if (n < 2) return n;
+//   return fib(n-1) + fib(n-2);
+// };
+
 var fib = function(n) {
-  if (n < 2) return n;
-  return fib(n-1) + fib(n-2);
+  var mem = {};
+
+  var helper = function(n, mem) {
+    if (n < 2) {
+      mem[n] = n;
+    }
+    else if (!mem[n]) {
+      mem[n] = helper(n-1, mem) + helper(n-2, mem);
+    }
+    return mem[n];
+  }
+
+  return helper(n, mem);
 };
+
+// var fib = function(n) {
+//   const f = {};
+//   f[0] = 0;
+//   f[1] = 1;
+
+
+//   for (let i = 2; i < n + 1; i++) {
+//     f[i] = f[i-2] + f[i-1];
+//   }
+
+//   return f[n];
+// }
 
 module.exports = fib;
