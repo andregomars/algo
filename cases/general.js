93ea859c6c4441054bed9019b9ca463e9926e6fb
andre <andregomars@gmail.com>
2020-06-06 18:14:15 -0700

    test 5

--- cases/general.js
+++ cases/general.js
@@ -26,13 +26,43 @@ Output: "bb"
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(s) {
-    
+var longestPalindrome = function (s) {
+    let max = 0;
+    let start = 0;
+    for (let i = 0; i < s.length; i++) {
+        const odd = palLen(s, i, i);
+        const even = palLen(s, i, i + 1);
+        max = Math.max(max, even, odd);
+        
+        if (max === even) {
+            start = i - (max/2 - 1);
+        } else if (max === odd) {
+            start = i - (max-1)/2;
+        }
+    }
+
+    return s.substring(start, max + start);
 };
 
-var test = () => {
-    console.log('general')
+var palLen = (s, left, right) => {
+    if (s.length < 1 || left > right) return 0;
 
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1;
+}
+
+
+var test = () => {
+    // console.log(palLen('adda', 1, 1))
+    // console.log(palLen('adda', 1, 2))
+    // console.log(palLen('adada', 2, 2))
+    // console.log(longestPalindrome('adda'))
+    // console.log(longestPalindrome('babab'))
+    // console.log(longestPalindrome('babad'))
+    console.log(longestPalindrome('civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'))
 }
 
 module.exports.test = test;
